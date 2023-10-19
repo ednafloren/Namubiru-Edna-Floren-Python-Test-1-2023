81bcd10a5c713bcdc267604289cdda746a7738a5
ednafloren <ednafloren27@gmail.com>
2023-10-19 09:20:12 +0300

    question2

--- question_one.py
+++ question_one.py
@@ -1,9 +1,16 @@
 # i)a)
+# snakecase
 # student_marks
+# camelcase
 # studentMarks
 
 # b)
+# snakecase
 # total_average_mark
+# camelcase
 # tatalAverageMark
 
 # ii)
+age=22
+newAge=float(age)
+print(type(newAge))
---
+++ question_two.py
@@ -0,0 +1,5 @@
+def averageMark(x,y):
+    sum=x+y
+    average=sum/2
+    print(f'The average of the two numbers is {average}')
+averageMark(2,4)    
